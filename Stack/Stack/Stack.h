ae77396c7c8302656eed36f1b9e2760218f1f7dc
Suki Lo <sukiwuikilo@gmail.com>
2018-01-22 22:09:23 -0800

    added default constructor, push function

---
+++ Stack/Stack/Stack.h
@@ -0,0 +1,23 @@
+class Stack{
+
+constexpr int MAX_SIZE{10};
+
+private:
+	int array [10];
+	int top;
+
+public:
+	//Default Constructor
+	Stack() {
+		top = -1; 
+	}
+
+	void push(int value) {
+		array[top + 1] = value;
+		top++; 
+	}
+
+
+
+
+};
